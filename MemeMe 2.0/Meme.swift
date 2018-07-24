8790bb59a143657d1b611f0403ea3fd9c2c4938f
Shehryarbajwa <shehryar.bajwa@gmail.com>
2018-07-24 18:43:16 -0400

    Meme Struct

---
+++ MemeMe 2.0/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  MemeMe 2.0
+//
+//  Created by Shehryar Bajwa on 2018-09-01.
+//  Copyright © 2018 Shehryar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct Meme {
+    var top : String = ""
+    var bottom : String = ""
+    var image : UIImage?
+    var memedImage : UIImage?
+}
