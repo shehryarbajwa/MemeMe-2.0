fcc2fb8469a9ccd879cf036cfeaf27298f66c37b
Shehryarbajwa <shehryar.bajwa@gmail.com>
2018-08-07 19:33:11 -0400

    Detail Meme ViewController

--- MemeMe 2.0/DetailMemeViewController.swift
+++ MemeMe 2.0/DetailMemeViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class DetailMemeVC: UIViewController {
+class DetailMemeViewController: UIViewController {
     
     @IBOutlet weak var imgView: UIImageView!
     
